39e0b0d0c63b37075a5467779d2c13d54637ab20
rodzy <49137701+rodzy@users.noreply.github.com>
2020-06-29 23:25:13 -0600

    Recoloring 🦄

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -8,11 +8,11 @@ export class IndexComponent implements OnInit {
   public slides = [
     {
       src:
-        'https://images.unsplash.com/photo-1565098772267-60af42b81ef2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1384&q=80',
+        'https://images.unsplash.com/photo-1559813800-e72c62ba08e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
     },
     {
       src:
-        'https://images.unsplash.com/photo-1559813800-e72c62ba08e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+        'https://images.unsplash.com/photo-1565098772267-60af42b81ef2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1384&q=80',
     },
   ];
 
