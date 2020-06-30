a7b69bee12f2fa6ac6145a2abe76bb0eda5220d7
rodzy <49137701+rodzy@users.noreply.github.com>
2020-06-29 22:26:11 -0600

    Revamp login and updates on users modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from './components/shared/shared.module';
 import { PagesModule } from './components/pages/pages.module';
 import { UsersModule } from './components/users/users.module';
 
-
-const declarations: any[] = [AppComponent, LoginComponent];
+const declarations: any[] = [AppComponent];
 
 const imports: any[] = [
   BrowserModule,
@@ -18,7 +15,7 @@ const imports: any[] = [
   BrowserAnimationsModule,
   SharedModule,
   PagesModule,
-  UsersModule
+  UsersModule,
 ];
 
 @NgModule({
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const declarations: any[] = [DashboardComponent, RegisterComponent];
-const exports: any[] = [DashboardComponent, RegisterComponent];
+const declarations: any[] = [
+  DashboardComponent,
+  RegisterComponent,
+  LoginComponent,
+];
+const exports: any[] = [DashboardComponent, RegisterComponent, LoginComponent];
 
 @NgModule({
   declarations: [...declarations],
