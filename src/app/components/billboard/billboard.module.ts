1f7b27a8301834e48dea42b3d0b64464680d506e
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-01 13:04:10 -0600

    Added more routing and modules

---
+++ src/app/components/billboard/billboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BillboardRoutingModule } from './billboard-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BillboardRoutingModule
+  ]
+})
+export class BillboardModule { }
