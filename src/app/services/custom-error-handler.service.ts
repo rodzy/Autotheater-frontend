1f8b939f667be2ff4fc205f3ff81b3a5336b2ef3
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-02 13:59:54 -0600

    Error handling service done 📨

--- src/app/services/custom-error-handler.service.ts
+++ src/app/services/custom-error-handler.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+export interface IError {
+  error: {
+    message: string;
+    errors: {
+      field: string;
+      message: string;
+    }[];
+  };
+  message: string;
+  name: string;
+  ok: boolean;
+  status: number;
+  statusText: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomErrorHandlerService {
+  constructor() {}
 
-  constructor() { }
+  public handleErrors(error: IError | HttpErrorResponse) {
+    if (error instanceof ErrorEvent) {
+      console.error('Error', error.message);
+    }
+    if (error.status === 404) {
+      error.error.message = 'Recurso no encontrado';
+    }
+    if (error.status === 401) {
+      error.error.message = 'No autorizado';
+    }
+    if (error.status === 400) {
+      error.error.message = 'Solicitud incorrecta';
+    }
+    return throwError(error);
+  }
 }
