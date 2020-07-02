151a9cab1e76bda0677ea2e018ab49c879cfbc8f
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-02 15:42:05 -0600

    Basic listing and object manipulation service done

--- src/app/services/generic.service.ts
+++ src/app/services/generic.service.ts
@@ -3,6 +3,8 @@ import { environment } from '../../environments/environment';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { CustomErrorHandlerService } from './custom-error-handler.service';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -32,5 +34,37 @@ export class GenericService {
         );
       }
     }
+  }
+
+  // Listing
+  List(endpoint: string): Observable<any> {
+    return this.http
+      .get<any>(this.server + endpoint, { headers: this.headers })
+      .pipe(catchError(this.handler.handleErrors.bind(this)));
+  }
+
+  // Obtain a resource
+  Obtain(endpoint: string, filter: any): Observable<any | any[]> {
+    return this.http
+      .get<any | any[]>(this.server + endpoint + `/${filter}`, {
+        headers: this.headers,
+      })
+      .pipe(catchError(this.handler.handleErrors.bind(this)));
+  }
+
+  // Create a resource
+  Create(endpoint: string, object: any | any): Observable<any | any[]> {
+    return this.http.post<any | any[]>(this.server + endpoint, object, {
+      headers: this.headers,
+    });
+  }
+
+  // Patch a resource
+  Update(endpoint: string, object: any | any[]): Observable<any | any[]> {
+    return this.http.patch<any | any[]>(
+      this.server + endpoint + `/${object.id}`,
+      object,
+      { headers: this.headers }
+    );
   }
 }
