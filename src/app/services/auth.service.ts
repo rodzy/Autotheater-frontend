97671f64ab6aee45f2dfcfd132c48e0b5a564f40
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-02 14:19:04 -0600

    Auth service getting the current user instance

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CustomErrorHandlerService } from './custom-error-handler.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  // Stablishing headers for the http requests
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+  server = environment.SERVER_URL;
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+
+  constructor(
+    private http: HttpClient,
+    private handler: CustomErrorHandlerService
+  ) {
+    this.currentUserSubject = new BehaviorSubject<any>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  // Getting the current user information
+  public getCurrentUserInfo(): any {
+    return this.currentUserSubject.value;
+  }
 }
