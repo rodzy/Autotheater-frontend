e67f9f20a8d87ad8a36291a8a6f8a32e0ab1bab0
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-06 20:57:11 -0600

    Locations requests finished 📡

--- src/app/components/pages/locations/locations.component.ts
+++ src/app/components/pages/locations/locations.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { GenericService } from '../../../services/generic.service';
 import { NotficationService } from '../../../services/notfication.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-locations',
@@ -13,9 +14,31 @@ export class LocationsComponent implements OnInit {
   error: any;
   destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(
-    private gServirce: GenericService,
+    private gService: GenericService,
     private notification: NotficationService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.listingLocations();
+  }
+
+  listingLocations() {
+    this.gService
+      .List('locations/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (data: any) => {
+          this.data = data;
+        },
+        (error: any) => {
+          this.notification.message(error.name, error.messge, 'error');
+        }
+      );
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
