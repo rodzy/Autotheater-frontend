0b7145b725b0d7b53ae3aef104c7f0c70fd423f6
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-08 13:59:56 -0600

    [Fix] Swiper not working

--- src/app/components/movies/movies.module.ts
+++ src/app/components/movies/movies.module.ts
@@ -4,13 +4,11 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { IndexComponent } from './index/index.component';
 import { UpcomingmoviesComponent } from './upcomingmovies/upcomingmovies.component';
 import { SharedModule } from '../shared/shared.module';
-import { SwiperComponent } from './swiper/swiper.component';
 import {NgxUsefulSwiperModule} from 'ngx-useful-swiper';
 
 const declarations: any[] = [
   IndexComponent,
   UpcomingmoviesComponent,
-  SwiperComponent,
 ];
 const imports: any[] = [
   CommonModule,
