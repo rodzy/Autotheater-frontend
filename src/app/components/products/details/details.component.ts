364c4a8748bb7ddcfecb2138d61c87872e35228a
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-15 17:11:52 -0600

    Functional product details 👍

--- src/app/components/products/details/details.component.ts
+++ src/app/components/products/details/details.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { GenericService } from '../../../services/generic.service';
+import { NotficationService } from '../../../services/notfication.service';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  data: any;
+  errors: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(
+    private gService: GenericService,
+    private notification: NotficationService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.ObtainProductDetails(id);
   }
 
+  // Listing movies using the generic service and the notifying service
+  ObtainProductDetails(id: any) {
+    this.gService
+      .Obtain('products', id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (data: any) => {
+          this.data = data;
+        },
+        (error: any) => {
+          this.notification.message(error.name, error.messge, 'error');
+        }
+      );
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
--- src/app/components/products/products-routing.module.ts
+++ src/app/components/products/products-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
-
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: 'products',
     component: IndexComponent,
   },
+  {
+    path: 'products/:id',
+    component: DetailsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
