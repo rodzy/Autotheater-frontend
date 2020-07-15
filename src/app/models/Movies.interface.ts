af788e0614f070478a31b3e382e4f4ce7e85d63a
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-15 12:24:23 -0600

    Updates on movies model

--- src/app/models/Movies.interface.ts
+++ src/app/models/Movies.interface.ts
@@ -1,4 +1,4 @@
-export default interface Movie {
+export interface Movie {
   id: number;
   name: string;
   sinopsis: string;
@@ -8,5 +8,20 @@ export default interface Movie {
   classification_id: number;
   created_at: string;
   updated_at: string;
-  likes_count: number;
+  likes_count?: number;
+  genres: Genre[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  pivot: Pivot;
+}
+
+export interface Pivot {
+  movie_id: number;
+  genre_id: number;
 }
