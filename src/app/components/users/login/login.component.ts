3cdee5e6db0e67e3af76eeaac07205a144d64782
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-27 18:00:41 -0600

    Added login form validation

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Login } from '../../../models/Login.interface';
+import { AuthService } from '../../../services/auth.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  user: any;
+  error: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  login: Login;
+  LoginForm: FormGroup;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  reactiveForm() {
+    this.LoginForm = new FormGroup({
+      email: new FormControl(this.login.email, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.email,
+      ]),
+      password: new FormControl(this.login.password, [
+        Validators.required,
+        Validators.minLength(12),
+      ]),
+    });
   }
 
+  onSubmited() {
+    if (this.LoginForm.invalid) {
+      return;
+    }
+    this.authService
+      .LoginUser(this.LoginForm.value)
+      .subscribe((res: any) => {});
+  }
+  updateValues() {}
 }
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UsersRoutingModule } from './users-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,9 +13,11 @@ const declarations: any[] = [
 ];
 const exports: any[] = [DashboardComponent, RegisterComponent, LoginComponent];
 
+const imports: any[] = [CommonModule, UsersRoutingModule, ReactiveFormsModule];
+
 @NgModule({
   declarations: [...declarations],
   exports: [...exports],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [...imports],
 })
 export class UsersModule {}
