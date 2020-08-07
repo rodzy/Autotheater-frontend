a825fe6c54e9e1cc1b3ce6f4c90bd25d4f5b977c
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-07 11:27:54 -0600

    Updated administration routing module

--- src/app/components/administration/administration-routing.module.ts
+++ src/app/components/administration/administration-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateMoviesComponent } from './create-movies/create-movies.component';
 import { CreateProductsComponent } from './create-products/create-products.component';
+import { UpdateMoviesComponent } from './update-movies/update-movies.component';
+import { UpdateProductsComponent } from './update-products/update-products.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: '/create-products',
     component: CreateProductsComponent,
+  },
+  {
+    path: '/update-movies',
+    component: UpdateMoviesComponent,
+  },
+  {
+    path: '/update-products',
+    component: UpdateProductsComponent,
   },
 ];
 
