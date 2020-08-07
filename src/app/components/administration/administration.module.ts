a3c671e78dd3a7200e6a449fb246633a2bb33fc8
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-07 11:24:13 -0600

    Add(Update-movies) @ administration components

--- src/app/components/administration/administration.module.ts
+++ src/app/components/administration/administration.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { CreateMoviesComponent } from './create-movies/create-movies.component';
 import { CreateProductsComponent } from './create-products/create-products.component';
+import { UpdateMoviesComponent } from './update-movies/update-movies.component';
 
-const declarations: any = [CreateMoviesComponent, CreateProductsComponent];
+const declarations: any = [CreateMoviesComponent, CreateProductsComponent, UpdateMoviesComponent];
 const imports: any = [CommonModule, AdministrationRoutingModule];
 
 @NgModule({
---
+++ src/app/components/administration/update-movies/update-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-movies',
+  templateUrl: './update-movies.component.html',
+  styleUrls: ['./update-movies.component.scss']
+})
+export class UpdateMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
