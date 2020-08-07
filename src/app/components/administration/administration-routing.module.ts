e70a2ee861a8328f6b0c33a49b6c819f723abcf2
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-07 11:20:57 -0600

    Added routes for the administration module

--- src/app/components/administration/administration-routing.module.ts
+++ src/app/components/administration/administration-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateMoviesComponent } from './create-movies/create-movies.component';
+import { CreateProductsComponent } from './create-products/create-products.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/create-movies',
+    component: CreateMoviesComponent,
+  },
+  {
+    path: '/create-products',
+    component: CreateProductsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdministrationRoutingModule { }
+export class AdministrationRoutingModule {}
