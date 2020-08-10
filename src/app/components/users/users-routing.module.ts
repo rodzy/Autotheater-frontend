16d8f6432cbade02c52a4ee9b0fd87d6e48d3215
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-09 19:40:45 -0600

    Added role guard for admins 🛅

--- src/app/components/users/users-routing.module.ts
+++ src/app/components/users/users-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardService } from '../../guards/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [AuthGuardService],
   },
 ];
 
