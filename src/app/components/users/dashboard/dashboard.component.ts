c592bc2060b8b1abf9b84c2fc7c42dad669415d9
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-10 15:57:59 -0600

    Added shared module integrations with users modules

--- src/app/components/users/dashboard/dashboard.component.ts
+++ src/app/components/users/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from '../../../models/Users.interface';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  user: Users;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.user = this.authService.getCurrentUserInfo().user;
+    console.log(this.user);
   }
-
 }
