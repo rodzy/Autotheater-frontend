069f9edfac24e7f56ccc83da369dc302152b7a48
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-15 16:19:19 -0600

    Bilboards movies filter done ✅

--- src/app/components/billboard/billboardindex/billboardindex.component.ts
+++ src/app/components/billboard/billboardindex/billboardindex.component.ts
@@ -4,6 +4,7 @@ import { NotficationService } from '../../../services/notfication.service';
 import { Billboard } from 'src/app/models/Bilboard.interface';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { Movie } from '../../../models/Movies.interface';
 
 @Component({
   selector: 'app-billboardindex',
@@ -14,6 +15,8 @@ export class BillboardindexComponent implements OnInit {
   data: Billboard[];
   location1: Billboard[];
   location2: Billboard[];
+  movieLocation1: Movie[];
+  movieLocation2: Movie[];
   show = null;
   destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(
@@ -32,11 +35,36 @@ export class BillboardindexComponent implements OnInit {
     this.gService
       .List<Billboard>('billboard', this.data)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((billboards: Billboard[]) => {
-        this.data = billboards;
-        this.location1 = this.data.filter((item) => item.location_id === 1);
-        this.location2 = this.data.filter((item) => item.location_id === 2);
-      });
+      .subscribe(
+        (billboards: Billboard[]) => {
+          this.data = billboards;
+          this.location1 = this.data.filter((item) => item.location_id === 1);
+          this.location2 = this.data.filter((item) => item.location_id === 2);
+        },
+        (error: any) => {
+          this.notification.message(error.name, error.messge, 'error');
+        }
+      );
+  }
+
+  // Listing content required
+  listBillboardContent() {
+    this.location1.forEach((element) => {
+      this.gService
+        .Obtain<Movie>('movies', this.movieLocation1, element.movie_id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((movie: Movie) => {
+          this.movieLocation1.push(movie);
+        });
+    });
+    this.location2.forEach((element) => {
+      this.gService
+        .Obtain<Movie>('movies', this.movieLocation2, element.movie_id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((movie: Movie) => {
+          this.movieLocation2.push(movie);
+        });
+    });
   }
 
   // tslint:disable-next-line: use-lifecycle-interface
