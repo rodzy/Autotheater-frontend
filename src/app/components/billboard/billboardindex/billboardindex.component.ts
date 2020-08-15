3ffa5e697bee19bf0649c6096ec19f89715ebab4
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-15 15:56:49 -0600

    Added(Billboards listing and filtering) @ Billboard index

--- src/app/components/billboard/billboardindex/billboardindex.component.ts
+++ src/app/components/billboard/billboardindex/billboardindex.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GenericService } from '../../../services/generic.service';
+import { NotficationService } from '../../../services/notfication.service';
+import { Billboard } from 'src/app/models/Bilboard.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-billboardindex',
@@ -6,13 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billboardindex.component.scss'],
 })
 export class BillboardindexComponent implements OnInit {
-  data: any;
+  data: Billboard[];
+  location1: Billboard[];
+  location2: Billboard[];
   show = null;
-  constructor() {}
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(
+    private gService: GenericService,
+    private notification: NotficationService
+  ) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('currentUser')) {
       this.show = localStorage.getItem('currentUser');
     }
+  }
+
+  // Listing billboards, separatelly for each location in mind
+  listBillboards() {
+    this.gService
+      .List<Billboard>('billboard', this.data)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((billboards: Billboard[]) => {
+        this.data = billboards;
+        this.location1 = this.data.filter((item) => item.location_id === 1);
+        this.location2 = this.data.filter((item) => item.location_id === 2);
+      });
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 }
