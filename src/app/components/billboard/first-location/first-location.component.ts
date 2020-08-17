1bdf0fc263d1f3d6ff24b9af9b17ea6c3a5fcee1
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-17 12:43:00 -0600

    Added(Destructure components) @ billboard

---
+++ src/app/components/billboard/first-location/first-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-location',
+  templateUrl: './first-location.component.html',
+  styleUrls: ['./first-location.component.scss']
+})
+export class FirstLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/billboard/second-location/second-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-location',
+  templateUrl: './second-location.component.html',
+  styleUrls: ['./second-location.component.scss']
+})
+export class SecondLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
