64bea8642917f4cdc65e16d998f8a064fb957482
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-18 14:06:14 -0600

    Obtain billboard and respective movie ✅

--- src/app/components/reservations/create-reservation/create-reservation.component.ts
+++ src/app/components/reservations/create-reservation/create-reservation.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NotficationService } from '../../../services/notfication.service';
 import { GenericService } from '../../../services/generic.service';
 import { Billboard } from '../../../models/Bilboard.interface';
 import { Movie } from '../../../models/Movies.interface';
 import { Products } from '../../../models/Products.interface';
+import { Tickets } from '../../../models/Tickets.interface';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-create-reservation',
@@ -18,10 +21,13 @@ export class CreateReservationComponent implements OnInit {
   billboard: Billboard;
   movie: Movie;
   products: Products[] = [];
+  tickets: Tickets[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     public formBuilder: FormBuilder,
     private router: Router,
+    private route: ActivatedRoute,
     private notification: NotficationService,
     private genericService: GenericService
   ) {}
@@ -31,7 +37,35 @@ export class CreateReservationComponent implements OnInit {
   /* This sets the initial state for the
      Create reservations component
   */
-  getInitialView() {}
+  getInitialView() {
+    const billboardId = this.route.snapshot.paramMap.get('id');
+    if (billboardId.trim() !== '') {
+      this.genericService
+        .Obtain<Billboard>('billboard', this.billboard, billboardId)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(
+          (bill: Billboard) => {
+            this.billboard = bill;
+          },
+          (error: any) => {
+            this.notification.message(error.name, error.messge, 'error');
+          }
+        );
+    }
+    if (this.billboard !== undefined) {
+      this.genericService
+        .Obtain<Movie>('movies', this.movie, this.billboard.id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(
+          (movie: Movie) => {
+            this.movie = movie;
+          },
+          (error: any) => {
+            this.notification.message(error.name, error.messge, 'error');
+          }
+        );
+    }
+  }
 
   /* This saves products on behalf of
      the user needs
@@ -57,4 +91,10 @@ export class CreateReservationComponent implements OnInit {
      users product insertions and tickets selections
   */
   onSubmitedReservation() {}
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
