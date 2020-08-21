44f4da96b61dd61300ba283c96abe7bf3d762287
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-21 10:45:21 -0600

    Added listing movies method

--- src/app/components/movies/check-movies/check-movies.component.ts
+++ src/app/components/movies/check-movies/check-movies.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../../models/Movies.interface';
+import { Subject } from 'rxjs';
+import { NotficationService } from '../../../services/notfication.service';
+import { GenericService } from '../../../services/generic.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-check-movies',
   templateUrl: './check-movies.component.html',
-  styleUrls: ['./check-movies.component.scss']
+  styleUrls: ['./check-movies.component.scss'],
 })
 export class CheckMoviesComponent implements OnInit {
-
-  constructor() { }
+  data: Movie[];
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(
+    private gService: GenericService,
+    private notification: NotficationService
+  ) {}
 
   ngOnInit(): void {
+    this.listingMovies();
   }
 
+  // Listing movies using the generic service and the notifying service
+  listingMovies() {
+    this.gService
+      .List<Movie>('movies/', this.data)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (data: Movie[]) => {
+          this.data = data;
+        },
+        (error: any) => {
+          this.notification.message(error.name, error.messge, 'error');
+        }
+      );
+  }
 }
